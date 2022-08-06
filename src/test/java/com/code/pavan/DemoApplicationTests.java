18069eeeae1add99c222eb55d007d6155791bac1
Pavan Kalyan <pavankalyanmagam@gmail.com>
2022-08-06 18:25:04 +0530

    First commit

---
+++ src/test/java/com/code/pavan/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.code.pavan;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(true, true);
+	}
+
+}
